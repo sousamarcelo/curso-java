5e894088a9ccba5a1d3aa50325ec896f660b0e25
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2020-10-02 16:51:26 -0300

    Praticando secao-4: Estrutura sequencial

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Ola mundo!");
+		System.out.print("Ola mundo!");
+		System.out.println("Bom dia");
+		
+		//imprimindo variavel
+		int y = 2;
+		System.out.println(y);
+		
+		//variavel ponto flutuante
+		double x = 10.5784;
+		System.out.println(x);
+		Locale.setDefault(Locale.US);
+		//imprindo formatado ja com ajusto do ponto no comando acima
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		System.out.println("Resultado " + x + " metros");
+		System.out.printf("Resultado = %.2f metros%n", x);
+		
+		// testando formatados maiores
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4000.0;
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade,renda);
 
 	}
 
